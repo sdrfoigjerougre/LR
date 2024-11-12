05f0794213eec50c8923dfe133f004fec5ba6141
Uxq <Uxq@DESKTOP-1BOQBO1>
2024-11-12 18:41:38 +0300

    Добавьте файлы проекта.

---
+++ MDK-01.01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDK_01._01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
